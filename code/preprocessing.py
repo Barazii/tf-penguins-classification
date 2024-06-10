58840ab92ccd399f4b065fca5903c21dd0fe1397
Mahmoud Barazi <mahmoud.baraziii@gmail.com>
2024-06-10 04:55:19 +0200

    create the transformers and transform the data

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -3,12 +3,31 @@ import pandas as pd
 from ..constants import *
 from sklearn.model_selection import train_test_split
 from pathlib import Path
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 
 def preprocess():
     df = _read_csv_data()
     train_df, test_df = train_test_split(df, train_size=0.8, test_size=0.2, random_state=1)
     _save_baseline(train_df, test_df)
+
+    # build the the column transformers
+    transformers = ColumnTransformer(
+        transformers=[
+            ("numeric", StandardScaler, make_column_selector(dtype_include="float64")),
+            ("categorical", OneHotEncoder, make_column_selector(dtype_include=object))
+        ],
+        verbose=True,
+    )
+    y_train = transformers.fit_transform(train_df.species.values)
+    y_test = transformers.transform(test_df.species.values)
+    train_df.drop(axis=1, columns="species")
+    test_df.drop(axis=1, columns="species")
+    X_train = transformers.fit_transform(train_df)
+    X_test = transformers.transform(test_df)
+
+    _save_splits(X_train, X_test, y_train, y_test)
 
 def _read_csv_data():
     csv_file = os.listdir(input_data_directory).pop()
@@ -21,6 +40,8 @@ def _save_baseline(train_df, test_df):
     train_df.to_csv(path_or_buf=path/f"train-baseline.csv", index=False, header=False)
     test_df.to_csv(path_or_buf=path/f"test-baseline.csv", index=False, header=False)
 
+def _save_splits(X_train, X_test, y_train, y_test):
+    pass
 
 if __name__ == "__main__":
     preprocess()
